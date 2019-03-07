98354ea9247d8d837a39190945b3e54acb45ff85
bbeyt <bbeyt@trinity.edu>
2019-03-07 08:21:37 -0600

    Express server now connects to a database. Requires a database config file with db info.

--- server/server.js
+++ server/server.js
@@ -1,12 +1,38 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const mysql = require('mysql');
+const dbConfig = require('./databaseConfig.js');
+
+const app = express();
+
+var connection = mysql.createConnection({
+  host     : dbConfig.host,
+  user     : dbConfig.user,
+  password : dbConfig.password,
+  database : dbConfig.database
+});
+
+//Connection test, left temporarily for reference's sake.
+connection.connect(function (err) {
+    if (err) throw err;
+});
+
+connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+    if (err) throw err;
+  
+    console.log('The solution is: ', rows[0].solution);
+  })
+
+connection.end(function (err) {
+    if (err) throw err;
+});
 
 app.get('/', function (req, res) {
    res.send('Welcome to the TU-Relay server.');
 })
 
 app.get('/new_messages', function (req, res) {
-    //TODO: Access DB and check for new messages
+    //TODO: Access DB and check for new messages with id higher than provided id.
+    //      If no provided id, provide all messages.
     res.send('This route will retrieve new messages.');
 })
 
@@ -15,9 +41,29 @@ app.post('/send_message', function (req, res) {
     res.send('This route will send a message.');
 })
 
-app.post('/make_category', function (req, res) {
-    //TODO: Parse category info from post and add to DB
-    res.send('This route will make a category.');
+app.delete('/delete_message', function (req, res) {
+    //TODO: Change specified message's delete flag to true
+    res.send('This route will delete a message.');
+})
+
+app.post('/make_type', function (req, res) {
+    //TODO: Parse type info from post and add to DB
+    res.send('This route will make a type.');
+})
+
+app.delete('/delete_type', function (req, res) {
+    //TODO: Delete the specified type from the database.
+    res.send('This route will delete a type.');
+})
+
+app.post('/make_template', function (req, res) {
+    //TODO: Parse template info from post and add to DB
+    res.send('This route will make a message template.');
+})
+
+app.delete('/delete_template', function (req, res) {
+    //TODO: Delete the specified template from the database
+    res.send('This route will delete a message template.');
 })
 
 var server = app.listen(8081, function () {
